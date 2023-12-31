c03f1becb9c51dbd199d5fdca9c417cd1e21503f
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-12-31 08:59:21 +0000

    Sun, Dec 31, 2023, 11:59 AM +03:00

--- LifeManager/test_project.py
+++ LifeManager/test_project.py
@@ -1,10 +1,14 @@
 import pytest
 from project import get_digit
 
-def test_valid_input():
+def test_valid_input(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _: '5')
+
     result = get_digit("Enter a digit: ", bot=1, top=10)
     assert 1 <= result <= 10
 
-def test_valid_input_without_top():
+def test_valid_input_without_top(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _: '3')
+
     result = get_digit("Enter a digit: ", bot=1)
     assert result >= 1
